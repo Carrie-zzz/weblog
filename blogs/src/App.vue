b3bb6d4930bb3c4e9849b1ed66030fa16e599357
tercel <924309827@qq.com>
2019-06-15 00:31:49 +0800

    首页

--- blogs/src/App.vue
+++ blogs/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png"> 我的页面
     <router-view/>
   </div>
 </template>
@@ -8,24 +7,19 @@
 <script>
 export default {
   name: 'App',
-  mounted () {
-    this.$http.get('/api/user/get').then(data => {
-      console.log(data)
-    })
-    this.$http.post('/api/user/post').then(data => {
-      console.log(data)
-    })
-  }
+  // mounted () {
+  //   this.$http.get('/api/user/get').then(data => {
+  //     console.log(data)
+  //   })
+  //   this.$http.post('/api/user/post').then(data => {
+  //     console.log(data)
+  //   })
+  // }
 }
 </script>
 <style lang='scss'>
   $color:yellow;
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: $color;
-    margin-top: 60px;
   }
 </style>
