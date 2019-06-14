771ccee57e1acff352f6e895a7767768d8e5b969
Carrie-zzz <1727479154@qq.com>
2019-06-14 09:34:27 +0800

    使用sass

--- blogs/src/App.vue
+++ blogs/src/App.vue
@@ -1,30 +1,31 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/logo.png"> 我的页面
     <router-view/>
   </div>
 </template>
 
 <script>
-export default{
-  name: 'App',
-  mounted () {
-    this.$http.get('/api/user/get').then(data => {
-      console.log(data)
-    })
-    this.$http.post('/api/user/post').then(data => {
-      console.log(data)
-    })
+  export default {
+    name: 'App',
+    mounted() {
+      this.$http.get('/api/user/get').then(data => {
+        console.log(data)
+      })
+      this.$http.post('/api/user/post').then(data => {
+        console.log(data)
+      })
+    }
   }
-}
 </script>
-<style>
-  #app{
+<style lang='scss'>
+  $color:red;
+  #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #2c3e50;
+    color: $color;
     margin-top: 60px;
   }
 </style>
